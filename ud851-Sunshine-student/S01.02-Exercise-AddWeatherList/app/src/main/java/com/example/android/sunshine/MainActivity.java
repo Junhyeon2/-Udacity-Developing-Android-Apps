ee50d0401c846f7d54d77dfa42fde4cf09d0ef73
Junhyeon KIM <junhyeon2@naver.com>
2017-01-07 21:31:09 +0900

    [Lesson_01] Proejct_02 Complete!!!

--- ud851-Sunshine-student/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ ud851-Sunshine-student/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,33 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // COMPLETE (1) Create a field to store the weather display TextView
+    TextView mWeatherDataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
-
-        // TODO (3) Create an array of Strings that contain fake weather data
-
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // COMPLETE (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherDataTextView = (TextView) findViewById(R.id.tv_weather_data);
+        // COMPLETE (3) Create an array of Strings that contain fake weather data
+        String[] fakeWeatherData = {
+                "2017-01-01(SUN) - 광역성 소나기 (2°C/8°C)",
+                "2017-01-02(MON) - 구름 조금 (-4°C/9°C)",
+                "2017-01-03(TUE) - 맑음 (-2°C/7°C)",
+                "2017-01-04(WED) - 구름 조금 (-1°C/8°C)",
+                "2017-01-05(THU) - 구름 조금 (-1°C/10°C)",
+                "2017-01-06(FRI) - 맑음 (-1°C/11°C)",
+                "2017-01-07(SAT) - 구름 조금 (2°C/10°C)",
+        };
+        // COMPLETE (4) Append each String from the fake weather data array to the TextView
+        for (String weatherData : fakeWeatherData) {
+            mWeatherDataTextView.append(weatherData + "\n\n\n");
+        }
     }
 }
